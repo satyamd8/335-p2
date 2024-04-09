99a5a1dc10e01bce236611c189a29e18120dceec
satyamd8 <satyamdhar8@gmail.com>
2024-04-09 15:15:36 -0400

    vector done

---
+++ main.cpp
@@ -0,0 +1,58 @@
+/*
+Name: Satyam Dhar
+Class: CSCI 335 Professor Tojeira
+Title: main.cpp
+Description: Converts input files to vector in a format readable for functions. Then passes vector to functions.
+Date: 4/5/2024
+*/
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include "myVector.hpp"
+#include "myList.hpp"
+#include "myHeap.hpp"
+#include "myAVLtree.hpp"
+using namespace std;
+
+int main(){
+    string input = "testinput.txt";
+    ifstream fin(input);
+
+    vector <int> instructions;
+
+    //include fail case
+    if (fin.fail()){
+        cout << "Could not open file" << endl;
+        exit(1);
+    }
+
+    string l;
+    while (getline(fin, l)){
+        istringstream word(l);
+        string inst;
+        int value; 
+
+        if (word >> inst){
+            if (inst == "insert"){
+                if (word >> value){
+                    instructions.push_back(value);
+                }
+            }
+            else if (inst == "pop"){ //MODIFY
+                string k;
+                if (word >> k && k == "median"){
+                    instructions.push_back(-1);
+                }
+                else{
+                    cout << "Can't pop" << endl; //temp
+                }
+            }
+        }
+        
+    }
+    fin.close();
+    vectorMedian(&instructions);
+    cout << "done" << endl;
+}
--- myVector.cpp
+++ myVector.cpp
@@ -8,6 +8,7 @@ Date: 4/5/2024
 
 #include "myVector.hpp"
 #include <iostream>
+#include <cmath>
 
 /*
     goes through instructions, 
@@ -19,17 +20,33 @@ void vectorMedian(const std::vector<int> * instructions)
     //FIND HOW TO USE LOWER_BOUND
     std::vector<int> temp;
 
-    for (int i = 0; i < instructions->size(); i++){
-        if (instructions->at(i) != 1){
-            auto it = std::lower_bound(instructions->begin(), instructions->end(), i);
-            temp.insert(it, instructions->at(i));
+    std::vector<int> medList;
+
+    for (auto it = instructions->begin(); it != instructions->end(); ++it){
+        if (*it != -1){
+            //std::cout << "push" << " ";//test
+
+            auto insert = std::lower_bound(temp.begin(), temp.end(), *it);
+            temp.insert(insert, *it);
         }
         else{
-            int median = temp.size()/2;
-            std::cout << temp[median] << " ";
+            int median;
+            if (temp.size() % 2 == 0){
+                median = temp.size() / 2 - 1;
+            }
+            else{
+                median = temp.size()/2;
+            }
+
+            medList.push_back(temp[median]);
             temp.erase(temp.begin() + median);
         }
     }
+
+    for (int i = 0; i < medList.size(); i++){
+        std::cout << medList[i] << " ";
+    }
+    
 }
 
 /* Copyright 2024, Satyam Dhar */
