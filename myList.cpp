78a635020ce485619ecd7369366c51465ce1981c
satyamd8 <satyamdhar8@gmail.com>
2024-04-09 17:16:11 -0400

    list

--- myList.cpp
+++ myList.cpp
@@ -9,19 +9,38 @@ Date: 4/5/2024
 #include "myList.hpp"
 #include <iostream>
 
-void listMedian(const std::vector<int> * instructions)
+void listMedian(const std::vector<int> * instructions) 
 {
-    std::list<int> temp;
-    for (int i = 0; i < instructions->size(); i++){
-        if (instructions->at(i) != -1){
-            //CANT USE LOWER_BOUND
-            //have to iterate through list
+    std::list<int> tempList;
+    std::vector<int> medList;
+
+    for (auto it = instructions->begin(); it != instructions->end(); ++it) {
+        if (*it != -1) {
+            auto insertPos = tempList.begin();
+            while (insertPos != tempList.end() && *insertPos < *it) {
+                ++insertPos;
+            }
+            tempList.insert(insertPos, *it);
+        } else {
+            int median;
+            if (tempList.size() % 2 == 0) {
+                auto it1 = tempList.begin();
+                std::advance(it1, tempList.size() / 2 - 1);
+                auto it2 = it1;
+                std::advance(it2, 1);
+                median = (*it1 + *it2) / 2;
+            } else {
+                auto itMedian = tempList.begin();
+                std::advance(itMedian, tempList.size() / 2);
+                median = *itMedian;
+            }
+            medList.push_back(median);
+            tempList.erase(std::next(tempList.begin(), tempList.size() / 2));
         }
-        else{
-            int median = temp.size()/2;
-            //must traverse through list to find median
-            //can use previous iteration
-        }
+    }
+
+    for (int i = 0; i < medList.size(); i++) {
+        std::cout << medList[i] << " ";
     }
 }
 
